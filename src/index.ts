fd43542b624b251a77c2658c978ef3d5fd684ab2
Claudio Barca <c.barca@libero.it>
2022-12-12 14:03:00 +0100

    comment fastify/env package

--- src/index.ts
+++ src/index.ts
@@ -31,46 +31,47 @@ const initialize = async () => {
     }
   })
 
-  // The schema is an object that describes the structure of the .env file.
-  const schema = {
-    type: 'object',
-    required: ['NODE_ENV', 'MONGO_URL'],
-    properties: {
-      NODE_ENV: {
-        type: 'string',
-        default: 'development'
-      },
-      MONGO_URL: {
-        type: 'string'
-      }
-    }
-  }
+  //TODO: I will remove the package fastify-env
+  // // The schema is an object that describes the structure of the .env file.
+  // const schema = {
+  //   type: 'object',
+  //   required: ['NODE_ENV', 'MONGO_URL'],
+  //   properties: {
+  //     NODE_ENV: {
+  //       type: 'string',
+  //       default: 'development'
+  //     },
+  //     MONGO_URL: {
+  //       type: 'string'
+  //     }
+  //   }
+  // }
 
-  // Basically, this tells Fastify how to access the environment file.
-  const options = {
-    /**
-     * The confKey specifies the name of the property
-     * that will contain the configuration.
-     */
-    confKey: 'config',
+  // // Basically, this tells Fastify how to access the environment file.
+  // const options = {
+  //   /**
+  //    * The confKey specifies the name of the property
+  //    * that will contain the configuration.
+  //    */
+  //   confKey: 'config',
 
-    dotenv: true,
+  //   dotenv: true,
 
-    /**
-     * Set the schema property to the schema object
-     */
-    schema: schema,
+  //   /**
+  //    * Set the schema property to the schema object
+  //    */
+  //   schema: schema,
 
-    /**
-     * Link it to the NodeJS process.env object
-     */
-    data: process.env
-  }
+  //   /**
+  //    * Link it to the NodeJS process.env object
+  //    */
+  //   data: process.env
+  // }
 
-  fastify.register(fastifyEnv, options)
+  // fastify.register(fastifyEnv, options)
 
-  await fastify.after()
-  // Now the .env variables are defined
+  // await fastify.after()
+  // // Now the .env variables are defined
 
   // Will load all plugins
   fastify.register(autoLoad, {
@@ -84,8 +85,6 @@ const initialize = async () => {
 
   try {
     await fastify.ready()
-    // console.log(fastify.printRoutes())
-    // console.log(fastify.config)
 
     await fastify.listen({ port: 5000 })
   } catch (err) {
